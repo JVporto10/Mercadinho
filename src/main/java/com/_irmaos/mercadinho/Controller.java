295824c29106c2c6a8df24025f2e1f4941bd42e9
João Victor <portojoaog@gmail.com>
2025-09-17 10:04:47 -0300

    criado a controller

---
+++ src/main/java/com/_irmaos/mercadinho/Controller.java
@@ -0,0 +1,15 @@
+package com._irmaos.mercadinho;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/BemVindo")
+    public String bemVindo (){
+        return "primeiro getMapping, bem vindo.";
+    }
+}
