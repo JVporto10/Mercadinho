efd7db0015bfcda3dbcfce1ebf40f61108e10ef4
João Victor <portojoaog@gmail.com>
2025-09-27 19:14:56 -0300

    cria uma entidade de banco de dados a partir do model

---
+++ src/main/java/com/_irmaos/mercadinho/MercadinhoModel.java
@@ -0,0 +1,48 @@
+package com._irmaos.mercadinho;
+
+import jakarta.persistence.*;
+
+// trasnfroma uma classe em uma entidade do banco de dados
+@Entity
+@Table(name = "tb_Produtos")
+public class MercadinhoModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+    String nome;
+    String categoria;
+    double valor;
+
+    public MercadinhoModel(){
+
+    }
+    public MercadinhoModel(String nome, String categoria, double valor) {
+        this.nome = nome;
+        this.categoria = categoria;
+        this.valor = valor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+}
